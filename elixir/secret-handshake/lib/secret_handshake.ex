26ff63c187ce518b186f1c4cf90b242471f3053b
cdemarti <codythomasdemartin@gmail.com>
2021-01-14 16:32:54 -0500

    solves secret handshake using Enum.zip and reduce

--- elixir/secret-handshake/lib/secret_handshake.ex
+++ elixir/secret-handshake/lib/secret_handshake.ex
@@ -15,5 +15,29 @@ defmodule SecretHandshake do
   """
   @spec commands(code :: integer) :: list(String.t())
   def commands(code) do
+    list = [
+      "wink",
+      "double blink",
+      "close your eyes",
+      "jump"
+    ]
+
+    digits = code
+    |> Integer.digits(2)
+
+    digits
+    |> Enum.reverse()
+    |> Enum.zip(list)
+    |> Enum.reduce( [], fn { number, value }, acc -> is_present(number, value, acc) end)
+    |> do_reverse(digits)
   end
+
+  defp is_present(1, value, accumulator ), do: [ value | accumulator ]
+  defp is_present(_, _, accumulator), do: accumulator
+
+  defp do_reverse( list, [1 | t ] ) when length(t) == 4, do: list
+  defp do_reverse(list, _ ), do: Enum.reverse(list)
+
+
+
 end
