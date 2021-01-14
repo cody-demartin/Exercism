884130b4c015c5f1cbd5eb2e715d5352c2f23b1f
cdemarti <codythomasdemartin@gmail.com>
2021-01-14 18:04:38 -0500

    solves rotational cipher

--- elixir/rotational-cipher/lib/rotational_cipher.ex
+++ elixir/rotational-cipher/lib/rotational_cipher.ex
@@ -1,4 +1,7 @@
 defmodule RotationalCipher do
+  @upper ?A..?Z
+  @lower ?a..?z
+
   @doc """
   Given a plaintext and amount to shift by, return a rotated string.
 
@@ -8,5 +11,19 @@ defmodule RotationalCipher do
   """
   @spec rotate(text :: String.t(), shift :: integer) :: String.t()
   def rotate(text, shift) do
+    text
+      |> to_charlist()
+      |> Enum.map(
+        &(rotate_char(&1, shift))
+      )
+      |> to_string()
+  end
+
+  defp rotate_char(char, shift) do
+    cond do
+      char in @upper -> rem(char - ?A + shift, 26) + ?A
+      char in @lower -> rem(char - ?a + shift, 26) + ?a
+      true           -> char
+    end
   end
 end
