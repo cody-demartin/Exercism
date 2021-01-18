504d5812a6539e537cd2435417aa1e6321ad71eb
cdemarti <codythomasdemartin@gmail.com>
2021-01-17 22:40:53 -0500

    some work on protein trans

--- elixir/protein-translation/lib/protein_translation.ex
+++ elixir/protein-translation/lib/protein_translation.ex
@@ -1,9 +1,31 @@
 defmodule ProteinTranslation do
+  @methionine "AUG"
+  @phenylalanine ["UUC", "UUU"]
+  @leucine ["UUA", "UUG"]
+  @serine ["UCU", "UCC", "UCA", "UCG"]
+  @tyrosine ["UAU", "UAC"]
+  @cysteine ["UGU", "UGC"]
+  @tryptophan "UGG"
+  @terminating ["UAA", "UAG", "UGA"]
+  @stop "STOP"
+  @codon_length ~r/.{3}/
+
+
   @doc """
   Given an RNA string, return a list of proteins specified by codons, in order.
   """
   @spec of_rna(String.t()) :: {atom, list(String.t())}
   def of_rna(rna) do
+    rna
+    |> String.split(~r/.{3}/, include_captures: true, trim: true)
+    |> Enum.map(fn x ->
+      result = of_codon(x)
+      if result === "STOP" do
+
+      end
+    end)
+    #map over the new chunks and pass the return value of of_codon back as we map
+
   end
 
   @doc """
@@ -29,5 +51,6 @@ defmodule ProteinTranslation do
   """
   @spec of_codon(String.t()) :: {atom, String.t()}
   def of_codon(codon) do
+    Map.get(@proteins, codon)
   end
 end
